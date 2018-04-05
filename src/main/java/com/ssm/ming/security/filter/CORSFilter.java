44e92ee5db5c915f1692c0b2f0c83d5caf0462ca
AOMING-H <haungwenming100@qq.com>
2018-04-06 00:22:16 +0800

    修改源码文件包位置

---
+++ src/main/java/com/ssm/ming/security/filter/CORSFilter.java
@@ -0,0 +1,51 @@
+/*
+ * Delta CONFIDENTIAL
+ *
+ * (C) Copyright Delta Electronics, Inc. 2015 All Rights Reserved
+ *
+ * NOTICE:  All information contained herein is, and remains the
+ * property of Delta Electronics. The intellectual and technical
+ * concepts contained herein are proprietary to Delta Electronics
+ * and are protected by trade secret, patent law or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Delta Electronics.
+ */
+
+package com.ssm.ming.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Wenming.Huang
+ * @date 2018-03-07
+ * @version 1.0
+ */
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig var1) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+            ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+    
+    @Override
+    public void destroy() {
+    }
+}
