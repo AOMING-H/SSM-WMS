2bb19b838171278adb62cc10817219b4dd37f146
AOMING-H <haungwenming100@qq.com>
2018-04-08 21:41:29 +0800

    1、完善物料信息管理页面的表格模板 2、修改主页实现方式

--- src/main/webapp/libs/model/common.js
+++ src/main/webapp/libs/model/common.js
@@ -2,4 +2,5 @@ $(function() {
 //	window.serverIP=window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '')+'/SSM-WMS/';
 	window.serverIP='http://localhost:8080/SSM-WMS/';
 	window.url_login = serverIP + 'login';
+	window.url_base_materials = serverIP + 'base/materials';
 })
